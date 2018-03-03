bb40e482647f64cfde70ed6beab8c02cdb7304a9
David Yu <equalbean@gmail.com>
2018-03-03 14:14:22 -0800

    Init the project

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by David Yu on 2018/3/3.
+//  Copyright © 2018年 David Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
