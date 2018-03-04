01afcbd4f9b20d72883a9806dac46a18736d1480
David Yu <equalbean@gmail.com>
2018-03-04 14:53:02 -0800

    init the cocoapods. fetch data from server. updated local data

--- Reddit/ViewController.swift
+++ Reddit/ViewController.swift
@@ -12,14 +12,65 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchData(success: {
+            print("success")
+            DispatchQueue.main.async {
+                
+            }
+            
+            print("cellDataArray = \(self.cellDataArray)")
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Fetch data from server
+    
+    private func fetchData(success: @escaping () -> ()) {
+        if let url = URL(string: "https://api.reddit.com") {
+            _ = URLSession.shared.dataTask(with: url, completionHandler : {data, response, error -> Void in
+                if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! NSDictionary {
+                    self.updateData(json: json)
+                    success()
+                } else {
+                    print("Cannot fetch data from server!")
+                }
+            }).resume()
+        } else {
+            print("The url is wrong!")
+        }
     }
-
-
+    
+    // MARK: - Updata local data
+    
+    private struct CellData {
+        let title: String
+        let author: String
+        let created: Int
+        let thumbnail: String
+        let numberOfComments: Int
+    }
+    
+    private var cellDataArray = [CellData]()
+    
+    private func updateData(json: NSDictionary) {
+        if let data = json["data"] as? NSDictionary, let children = data["children"] as? NSArray {
+            for child in children {
+                if let childDictionary = child as? NSDictionary {
+                    if let data = childDictionary["data"] as? NSDictionary,
+                        let title = data["title"] as? String,
+                        let author = data["author"] as? String,
+                        let created = data["created"] as? Int,
+                        let thumbnail = data["thumbnail"] as? String,
+                        let numberOfComments = data["num_comments"] as? Int {
+                        cellDataArray.append(CellData(title: title, author: author, created: created, thumbnail: thumbnail, numberOfComments: numberOfComments))
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
 }
 
